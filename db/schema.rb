5b98ea2992e6cb17b8c3d3076f4ba25bbc3aae01
Alejandro Barata <alexbgdev@gmail.com>
2021-07-15 09:59:29 +0200

    change migration schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_23_100638) do
+ActiveRecord::Schema.define(version: 2021_07_14_132929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
